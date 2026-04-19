0e79982ef9d0f6a917ae4954dd5ced66553b33a1
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-19 23:45:45 +0200

    first part realized

---
+++ level-1/index.php
@@ -0,0 +1,17 @@
+<?php
+//arrays//
+$numeros = [10,5,7,3,20];
+$language = ["PHP", "HTML", "CSS", "Java", "JavaScript", "Python"];
+
+echo "<h2> PARTE 1</h2>";
+
+//impresion de los elementos del array//
+foreach ($numeros as $numero) {
+         echo $numero . ",  ";
+}
+
+echo "<h2> PARTE 2</h2>";
+
+
+
+?> 
