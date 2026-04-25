c53eba6e8a2fe0a89448b1e6e7828cc486b6ab21
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-25 13:23:44 +0200

    add changes

--- level-1/parte-3/index.php
+++ level-1/parte-3/index.php
@@ -1,12 +1,10 @@
 <?php
 echo " PARTE 3 \n";
 
-//array de palabras//
 $palabras = ["hola", "clase", "manzana", "java", "hablar", "teclado", "maleta", "pantalla", "hacer", "cable"];
-//caracter//
+
 $char = "";
 
-//detectar caracteres//
 function detectarChar ( array $palabras , string $char): bool {
     $resultado = true;
     foreach ($palabras as $palabra) {
